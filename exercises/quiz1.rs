d6487d28abca1cf338d51597067e5813e2945429
Hong Viet Le <hvle@octo.com>
2021-11-14 23:54:42 +0100

    Training with Rust

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,11 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
-
+fn calculate_apple_price(nb_apples: i32) -> i32 {
+    if nb_apples > 40 {
+        return nb_apples;
+    } else {
+        return nb_apples * 2;
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
